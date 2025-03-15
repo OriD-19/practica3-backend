5d660601f178f06996f125e320010d2764933adf
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-03-15 07:38:13 -0600

    Finished this thing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRegisterRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -14,7 +13,7 @@ class UserController extends Controller
         $user = User::create($validated);
 
         return response()->json([
-            'message' => ['User created successfully'],
+            'message' => 'User created successfully',
             'user' => $user,
         ], 201);
     }
--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,12 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function() {
 
-    Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [UserController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 
     Route::middleware('auth:sanctum')->group(function() {
         Route::get('posts', [PostController::class, 'index']);
